dc61f7c734629753062ee547d1ca6c00329f0095
Dan Shrader <dan@shraderland.com>
2017-06-28 17:11:43 -0400

    Removed the stand alone package and status bar message
    Now that the extention is in the Chrome store I've removed the packaged plugin
    Also removed the status bar message as it was 'sticking' and didn't add much value

--- background.js
+++ background.js
@@ -15,5 +15,4 @@ chrome.runtime.onMessageExternal.addListener(
 		var options = message.options || {};
 		var result = js_beautify(data, options);
 		sendToCaret('editor:insert', result, true);
-		sendToCaret('status:set', "Completed JS Beautify'd the JavaScript", true);
 	});
